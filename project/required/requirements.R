0e0f7f620aa7eeb15797642c24a48b81e5366fec
Zinuo Jia <jiazinuo3@gmail.com>
2020-02-12 13:59:10 -0500

    setups

---
+++ project/required/requirements.R
@@ -0,0 +1,4 @@
+install.packages("data.table",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("optparse",repos= "http://lib.stat.cmu.edu/R/CRAN/")
+install.packages("devtools")
+devtools::install_github("DataComputing/DataComputing")
---
+++ project/src/features/build_features.r
@@ -0,0 +1,7 @@
+library("data.table")
+library(DataComputing)
+
+readDir = "./project/volume/data/raw"
+writeDir = "./project/volume/data/processed"
+exterDir = "./project/volume/data/external"
+interDir = "./project/volume/data/interim"
